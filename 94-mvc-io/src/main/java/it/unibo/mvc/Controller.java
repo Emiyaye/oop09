d79b6abcdb5fb23dea59b5b85fdcbf661d6d99de
Botti Bao - botti.bao@studio.unibo.it <botti.bao@studio.unibo.it>
2023-11-28 11:34:34 +0100

    finished SimpleController and deleted ControllerImpl

--- 94-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 94-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,8 +1,18 @@
 package it.unibo.mvc;
 
+import java.util.List;
+
 /**
  *
  */
 public interface Controller {
+
+    void setString(String s);
+
+    String getString();
+
+    List<String> history();
+
+    void printCurrentString();
 
 }
--- 94-mvc-io/src/main/java/it/unibo/mvc/SimpleController.java
+++ 94-mvc-io/src/main/java/it/unibo/mvc/SimpleController.java
@@ -1,9 +1,35 @@
 package it.unibo.mvc;
 
-/**
- * 
- *
- */
-public final class SimpleController implements Controller {
+import java.util.ArrayList;
+import java.util.List;
 
+public class SimpleController implements Controller {
+
+    private List<String> string = new ArrayList<>();
+    private int size;
+    
+    @Override
+    public void setString(String string) {
+        this.string.add(size, string);
+        size++;
+    }
+
+    @Override
+    public String getString() {
+        return new String(string.get(size));
+    }
+
+    @Override
+    public List<String> history() {
+        return new ArrayList<>(string);
+    }
+
+    @Override
+    public void printCurrentString() {
+        if (size == 0) {
+            throw new IllegalStateException("Ugabuga");
+        }
+        System.out.println(string.get(size));
+    }
+    
 }
