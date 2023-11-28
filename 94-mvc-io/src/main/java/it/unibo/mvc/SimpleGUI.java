468d83917c47489b5bd3a68add74b6de384a42f7
Botti Bao - botti.bao@studio.unibo.it <botti.bao@studio.unibo.it>
2023-11-28 12:04:15 +0100

    finished 94 (finished SimpleGUI)

--- 94-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
+++ 94-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
@@ -1,6 +1,12 @@
 package it.unibo.mvc;
 
+import java.awt.BorderLayout;
+import java.util.List;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 /**
  * A very simple program using a graphical interface.
@@ -8,6 +14,41 @@ import javax.swing.JFrame;
  */
 public final class SimpleGUI {
 
+    private final static int TEXTLENGHT = 20;
     private final JFrame frame = new JFrame();
+    private final Controller controller = new SimpleController();
 
+    public SimpleGUI() {
+        JPanel canvas = new JPanel();
+        JTextField myTextField = new JTextField(TEXTLENGHT);
+        canvas.setLayout(new BorderLayout());
+        canvas.add(myTextField, BorderLayout.CENTER);
+        JButton print = new JButton("Print");
+        JButton history = new JButton("Show History");
+        canvas.add(print, BorderLayout.WEST);
+        canvas.add(history, BorderLayout.EAST);
+        print.addActionListener(e -> {
+            controller.setString(myTextField.getText());
+            controller.printCurrentString();
+            myTextField.setText("");
+        });
+        history.addActionListener(e -> {
+            List<String> s = controller.history();
+            System.out.println(s);
+        });
+
+        frame.setContentPane(canvas);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setLocationByPlatform(true);
+    }
+
+    void display() {
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        final SimpleGUI gui = new SimpleGUI();
+        gui.display();
+    }
 }
